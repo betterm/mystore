3849f242b063201019291b3de59259e0c2d8e30e
Misha <mishakib91@icloud.com>
2015-10-10 19:23:35 +0300

    Finished cart
    Cart is finished

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,3 +1,11 @@
+#---
+# Excerpted from "Agile Web Development with Rails",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material, 
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose. 
+# Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
+#---
 class LineItemsController < ApplicationController
   include CurrentCart
   before_action :set_cart, only: [:create]
@@ -25,17 +33,13 @@ class LineItemsController < ApplicationController
 
   # POST /line_items
   # POST /line_items.json
-
-    def create
-      product = Product.find(params[:product_id])
-      @line_item = @cart.add_product(product.id)
-    end
-
+  def create
+    product = Product.find(params[:product_id])
+    @line_item = @cart.add_product(product.id)
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart,
-                                  notice: 'Line item was successfully created.' }
+        format.html { redirect_to @line_item.cart }
         format.json { render action: 'show',
                              status: :created, location: @line_item }
       else
@@ -52,9 +56,9 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.update(line_item_params)
         format.html { redirect_to @line_item, notice: 'Line item was successfully updated.' }
-        format.json { render :show, status: :ok, location: @line_item }
+        format.json { head :no_content }
       else
-        format.html { render :edit }
+        format.html { render action: 'edit' }
         format.json { render json: @line_item.errors, status: :unprocessable_entity }
       end
     end
@@ -65,19 +69,21 @@ class LineItemsController < ApplicationController
   def destroy
     @line_item.destroy
     respond_to do |format|
-      format.html { redirect_to line_items_url, notice: 'Line item was successfully destroyed.' }
+      format.html { redirect_to line_items_url }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_line_item
-      @line_item = LineItem.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_line_item
+    @line_item = LineItem.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def line_item_params
-      params.require(:line_item).permit(:product_id, :cart_id)
-    end
-
+  # Never trust parameters from the scary internet, only allow the white
+  # list through.
+  def line_item_params
+    params.require(:line_item).permit(:product_id)
+  end
+  #...
+end
